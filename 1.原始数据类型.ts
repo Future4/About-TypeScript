dbaa8dfb1fffc5ae9d35557bbe1659f13c901bb2
Future4 <38273351+Future4@users.noreply.github.com>
2020-05-26 20:43:15 +0800

    Create 1.原始数据类型.ts

---
+++ 1.原始数据类型.ts
@@ -0,0 +1,39 @@
+
+// 布尔值
+let flag:boolean = false;
+
+// new Boolean() 返回的是一个Boolean对象
+let creacreatedByNewBoolean: Boolean = new Boolean(1)
+
+// 数值
+let decLiteral: number = 6;
+let hexLiteral: number = 0xf00d;
+// ES6 中的二进制表示法
+let binaryLiteral: number = 0b1010;
+// ES6 中的八进制表示法
+let octalLiteral: number = 0o744;
+let notANumber: number = NaN;
+let infinityNumber: number = Infinity;
+
+
+// 字符串
+let myName:string = 'zhang';
+let myAge:number = 18;
+// 模板字符串
+let sentence:string = `Hello,my name is ${myName}.
+I'll be ${myAge + 1} years old next month.`
+
+
+// 空值
+function alertName(): void {
+  alert('My name is Tom');
+}
+
+let unusable: void = undefined;
+
+// Null Undefiend
+// 与viod不同的是，null和undefined是所有类型的子类型
+let u:number = undefined
+
+
+
