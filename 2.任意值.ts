c3e2d26ef698d0427a6babc84898bc4f87901c79
Future4 <38273351+Future4@users.noreply.github.com>
2020-05-27 21:30:12 +0800

    Update 2.任意值.ts

--- 2.任意值.ts
+++ 2.任意值.ts
@@ -1,8 +1,17 @@
 // 在赋值过程中改变类型是不可以的
 // 但是在any中就可以
-var myFavoriteNumber = 7;
-myFavoriteNumber = 'seven';
+
+let myFavoriteNumber: any = 7
+myFavoriteNumber = 'seven'
+
 //可以在任意值上访问任意方法和属性
-var anyThing = 'hello';
+let anyThing: any = 'hello';
 console.log(anyThing.myName);
 console.log(anyThing.myName.firstName);
+
+// 如果在声明变量时未指定类型，那么他会被识别成任意值类型
+let something;
+something = 'seven';
+something = 7;
+
+something.setName('Tom');
